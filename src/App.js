758e60f01be8aa0023880545239c23246305f088
Troy Dorman <troydorman@gmail.com>
2021-08-18 10:07:14 -0600

    fixed bug in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./assets/main.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
 import ImageSearch from "./components/ImageSearch";
 
@@ -10,31 +10,31 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}_type=photo&pretty=true`,
-      { mode: "cors" }
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
-      .then(
-        (result) => {
-          setImages(result.hits);
-          setIsLoading(false);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
   }, [term]);
 
   return (
     <div className="container mx-auto">
       <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
         <div className="grid grid-cols-3 gap-4">
-          {images.map((image) => {
-            return <ImageCard key={image.id} image={image} />;
-          })}
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
       )}
     </div>

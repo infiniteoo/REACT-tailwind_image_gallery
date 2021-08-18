578c4d15aca1b75e65106ac8b4b86b63656a1051
Troy Dorman <troydorman@gmail.com>
2021-08-18 09:09:01 -0600

    imported react hooks and created three state variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./assets/main.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />

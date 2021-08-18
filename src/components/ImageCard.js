1ce31b67dadb465a1a0f914700a2701321c85d68
Troy Dorman <troydorman@gmail.com>
2021-08-18 09:55:34 -0600

    gave the images a unique key with index value.  created loading screen while images load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -24,10 +24,10 @@ const ImageCard = ({ image }) => {
           </li>
         </ul>
       </div>
-      <div className="px-6-py-4">
-        {tags.map((tag) => (
+      <div className="px-6-py-4 my-4 mx-3">
+        {tags.map((tag, index) => (
           <span
-            key={image.id}
+            key={index}
             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2"
           >
             #{tag}
